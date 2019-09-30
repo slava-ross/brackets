2fe37a58442cf748ece57dbdcecf5479e9897e1f
Yaroslav Yagodarov <slava-ross@yandex.ru>
2019-09-30 21:16:32 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check( str, bracketsConfig ) {
+    let sourceArray = str.split( '' );
+    let idx = 0;
+    let isPair = false;
+    while ( idx < sourceArray.length - 1 ) {
+        let currBracket = sourceArray[ idx ];
+        let nextBracket = sourceArray[ idx + 1 ];
+        for ( let styleArray of bracketsConfig ) {
+            if ( currBracket === styleArray[0] && nextBracket === styleArray[1] ) {
+                sourceArray.splice( idx, 2 );
+                isPair = true;
+                break;
+            }
+        }
+        if ( isPair ) {
+            idx = 0;
+            isPair = false;
+        } else {
+            idx++;
+        }
+    }
+    return !( sourceArray.length );
 }
